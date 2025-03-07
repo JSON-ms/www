8568e432377f6c7e26c3cbfd536ed1a4f7692cb2
Danny Coulombe <contact@dannycoulombe.com>
2025-03-07 17:03:06 -0500

    #11 select/option/autocomplete/combobox/enums (single/multiple)

--- src/components/FieldHeader.vue
+++ src/components/FieldHeader.vue
@@ -15,10 +15,10 @@ const {
 <template>
   <v-list-subheader class="pr-3 w-100 d-block" style="min-height: 2rem">
     <div class="d-flex align-center justify-space-between">
-          <span>
-            <span v-if="field.required" class="mr-2 text-error">*</span>
-            <span>{{ field.label }}</span>
-          </span>
+      <span>
+        <span v-if="field.required" class="mr-2 text-error">*</span>
+        <span>{{ field.label }}</span>
+      </span>
       <v-chip v-if="field.type.includes('i18n')" label size="x-small">
         {{ locales[locale] }}
       </v-chip>
